9fb0e3cd6b381b77f286cfbe7f697dd7968b5d5e
nat6 <nat6dv@gmail.com>
2021-03-26 16:21:19 +0300

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resultArr = arr.slice(0);
+
+  // iterate over elements and handle control sequences
+  function iterateAndTransform(num) {
+    for (let i = 0; i < resultArr.length; i++) {
+      // skip numbers
+      if (typeof resultArr[i] === 'number') continue;
+
+      // control sequences cases
+      switch(resultArr[i]) {
+
+        case '--discard-prev':
+          (resultArr[i - 1] === undefined) ? resultArr.splice([i], 1) : resultArr.splice([i - 1], 2);
+          iterateAndTransform(i);
+          break;
+
+        case '--double-prev':
+          (resultArr[i - 1] === undefined) ? resultArr.splice([i], 1) : resultArr[i] = resultArr[i - 1];
+          break;
+
+        case '--discard-next':
+          if (resultArr[i + 1] === undefined) { 
+            resultArr.splice([i], 1)
+          }
+          else { 
+            resultArr.splice([i + 1], 1);
+            resultArr[i] = undefined;
+          }
+
+          iterateAndTransform(i);
+          break;
+
+        case '--double-next':
+          (resultArr[i + 1] === undefined) ? resultArr.splice([i], 1) : resultArr[i] = resultArr[i + 1];
+          break;
+      }
+    }
+    // delete undefined elements
+    resultArr.forEach((elem, index) => {
+      if (elem === undefined) {
+        resultArr.splice(index, 1);
+      }
+    })
+  }
+  iterateAndTransform(0);
+  return resultArr;
 };
