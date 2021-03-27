1ddc7554eea77a816995a93e084e444b10909742
nat6 <nat6dv@gmail.com>
2021-03-27 13:41:09 +0300

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	if (!options.hasOwnProperty('separator')) {
+		options.separator = '+';
+	 }
+	 if (!options.hasOwnProperty('additionSeparator') && options.hasOwnProperty('additionRepeatTimes')) {
+		options.additionSeparator = '|';
+	 }
+	 if (options.hasOwnProperty('addition') && options.addition === null) {
+			options.addition = 'null';
+	 }
+
+	 // заполняем массив нужным количеством дополнительных элементов
+	 let additionArray = new Array(options.additionRepeatTimes).fill(options.addition);
+
+	 // соединяем элементы в строку с additionSeparator
+	 let additionString = additionArray.join(options.additionSeparator);
+
+	 // соединяем начало строки с готовыми доп элементами
+	 let partOfString = str + additionString;
+
+	 // дублируем строку нужное кол-во раз в массиве
+	 let resultArray = new Array(options.repeatTimes).fill(partOfString);
+
+	 // соединяем готовую строку с помощью сепараторов
+	 let result = resultArray.join(options.separator);
+
+  return result;
 };
-  
