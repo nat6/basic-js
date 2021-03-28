c05d0ea182810a037db98eaf1c6a2c436bf68487
nat6 <nat6dv@gmail.com>
2021-03-28 20:05:08 +0300

    feat: add chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chainLinks: [],
+  result: '',
+
+  // returns the current chain length as a number;
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let length = this.chainLinks.length;
+    return this.length;
   },
+
+  // adds a link containing a string representation of the value to the chain;
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      value = '';
+    }
+    else if (!value) {
+      value = String(value);
+    }
+    this.chainLinks.push('( ' + value + ' )');
+    return this;
   },
+
+  //removes a chain link in the specified position;
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== 'number') {
+      this.chainLinks = [];
+      throw new Error('I can`t remove');
+    }
+    this.chainLinks.splice(position - 1, 1);
+    return this;
   },
+
+  //  reverses the chain;
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainLinks.reverse();
+    return this;
   },
+
+  // ends the chain and returns it
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result = this.chainLinks.join('~~');
+    this.chainLinks = [];
+    return this.result;
   }
 };
 
